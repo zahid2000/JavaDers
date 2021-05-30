c486a16c9b306c64ddba72f7a45cf48557c38b52
Zahid Mamedov <67357750+zahid2000@users.noreply.github.com>
2021-05-31 01:37:37 +0400

    30.05.2021

--- Java/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ Java/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -5,12 +5,35 @@ import java.util.List;
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
+import kodlamaio.northwind.entities.dtos.ProductWithCategoryDto;
 
 public interface ProductService {
+
 	DataResult<List<Product>> getAll();
+	
+	DataResult<List<Product>> getAllSorted();	
+
+	DataResult<List<Product>> getAll(int pageNo,int pageSize);
+	
 	Result add(Product product);
+
 	Result update(Product product);
+
 	Result delete(Product product);
-	
-	
+
+	DataResult<Product> getByProductName(String productName);
+
+	DataResult<Product> getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+
+	DataResult<List<Product>> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+
+	DataResult<List<Product>> getByCategoryIn(List<Integer> categories);
+
+	DataResult<List<Product>> getByProductNameContains(String productName);
+
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+
+	DataResult<List<Product>> getByNameAndCategoryId(String productName, int categoryId);
+
+	DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails();
 }
